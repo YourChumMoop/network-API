3647d41f622e99253c5c34f055845948da57be71
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2025-01-11 22:44:04 -0600

    built thought model

---
+++ src/models/Thought.ts
@@ -0,0 +1,75 @@
+import { Schema, model, ObjectId, type Document, Types } from 'mongoose';
+
+interface IThought extends Document {
+    thoughtText: string;
+    createdAt: Date;
+    username: string;
+    reactions: ObjectId[];
+};
+
+interface IReaction extends Document {
+    reactionID: ObjectId;
+    reactionBody: string;
+    username: string;
+    createdAt: Date;
+};
+const reactionSchema = new Schema<IReaction>(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    },
+    {
+        timestamps: true,
+        _id: false,
+    }
+);
+const thoughtSchema = new Schema<IThought>(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        timestamps: true,
+    },
+);
+
+thoughtSchema
+    .virtual('reationCount').get(function (this:any) {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought',thoughtSchema)
+
+export default Thought;
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,10 @@
 import { Schema, model, type Document } from 'mongoose';
 
 interface IUser extends Document {
-    username: string,
-    email: string,
-    thoughts: Schema.Types.ObjectId[],
-    friends: Schema.Types.ObjectId[],
+    username: string;
+    email: string;
+    thoughts: Schema.Types.ObjectId[];
+    friends: Schema.Types.ObjectId[];
 }
 
 const userSchema = new Schema<IUser>(
